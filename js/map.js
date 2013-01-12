ad612554d9dd5f03ed8bedebcb223e18e5b38f40
mpizza <mmm198219@gmail.com>
2013-01-12 14:37:19 +0800

    add orientation.js

--- js/map.js
+++ js/map.js
@@ -39,7 +39,16 @@ var Canvas = {
   },
   
   kill_obstacles : function kill_obstacles(index){
-    this.obstacles.splice(index, 1);
+    var l = this.obstacles.length;
+    for (var i = 0; i < l; i++) {
+      var obstacle = this.obstacles[i];
+      if (obstacle.attr_id ==  index){
+        this.obstacles.splice(index, 1);
+        this.valid = false;
+        this.draw();
+        break;
+      }
+    }
   },
   
   draw_obstacles : function draw_obstacles(){
@@ -67,5 +76,35 @@ var Canvas = {
       this.draw_obstacles();
       this.valid = false; // reset valid
     }// draw it
+  },
+  
+  deviceMotionHandler: function deviceMotionHandler(eventData) {
+    // Grab the acceleration including gravity from the results
+    var acceleration = eventData.accelerationIncludingGravity;
+  
+    // Display the raw acceleration data
+    var rawAcceleration = "[" +  Math.round(acceleration.x) + ", " + Math.round(acceleration.y) + ", " + Math.round(acceleration.z) + "]";
+  
+    // Z is the acceleration in the Z axis, and if the device is facing up or down
+    var facingUp = -1;
+    if (acceleration.z > 0) {
+      facingUp = +1;
+    }
+    
+    // Convert the value from acceleration to degrees acceleration.x|y is the 
+    // acceleration according to gravity, we'll assume we're on Earth and divide 
+    // by 9.81 (earth gravity) to get a percentage value, and then multiply that 
+    // by 90 to convert to degrees.                                
+    var tiltLR = Math.round(((acceleration.x) / 9.81) * -90);
+    var tiltFB = Math.round(((acceleration.y + 9.81) / 9.81) * 90 * facingUp);
+  
+    // Display the acceleration and calculated values
+    document.getElementById("moAccel").innerHTML = rawAcceleration;
+    document.getElementById("moCalcTiltLR").innerHTML = tiltLR;
+    document.getElementById("moCalcTiltFB").innerHTML = tiltFB;
+  
+    // Apply the 2D rotation and 3D rotation to the image
+    //var rotation = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB) + "deg)";
+    //document.getElementById("imgLogo").style.webkitTransform = rotation;        
   }
 }// end of CanvasState
---
+++ js/orientation.js
@@ -0,0 +1,40 @@
+'use strict';
+if (window.DeviceMotionEvent) {
+  window.addEventListener('devicemotion', deviceMotionHandler, false);
+} else {
+  document.getElementById("dmEvent").innerHTML = "Not supported on your device."
+}
+
+function deviceMotionHandler(eventData) {
+  // Grab the acceleration including gravity from the results
+  var acceleration = eventData.accelerationIncludingGravity;
+
+  // Display the raw acceleration data
+  var rawAcceleration = "[" +  Math.round(acceleration.x) + ", " + Math.round(acceleration.y) + ", " + Math.round(acceleration.z) + "]";
+
+  // Z is the acceleration in the Z axis, and if the device is facing up or down
+  var facingUp = -1;
+  if (acceleration.z > 0) {
+    facingUp = +1;
+  }
+  
+  // Convert the value from acceleration to degrees acceleration.x|y is the 
+  // acceleration according to gravity, we'll assume we're on Earth and divide 
+  // by 9.81 (earth gravity) to get a percentage value, and then multiply that 
+  // by 90 to convert to degrees.                                
+  var tiltLR = Math.round(((acceleration.x) / 9.81) * -90);
+  var tiltFB = Math.round(((acceleration.y + 9.81) / 9.81) * 90 * facingUp);
+
+  // Display the acceleration and calculated values
+  document.getElementById("moAccel").innerHTML = rawAcceleration;
+  document.getElementById("moCalcTiltLR").innerHTML = tiltLR;
+  document.getElementById("moCalcTiltFB").innerHTML = tiltFB;
+  if( Canvas.car = null) {
+    Canvas.car.x = Canvas.car.x + Math.round(acceleration.x);
+    Canvas.valid = true;
+    console.log(Canvas.car.x + "update");
+  }
+  // Apply the 2D rotation and 3D rotation to the image
+  //var rotation = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB) + "deg)";
+  //document.getElementById("imgLogo").style.webkitTransform = rotation;        
+}
